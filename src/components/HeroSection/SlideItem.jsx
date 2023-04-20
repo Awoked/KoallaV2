00cb7f026fba9eb139c1061377af759b18c99607
Awoked <alper.kossay@gmail.com>
2023-04-20 16:16:41 +0300

    update - slider animations

--- src/components/HeroSection/SlideItem.jsx
+++ src/components/HeroSection/SlideItem.jsx
@@ -1,18 +1,63 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import Image from 'next/image';
 
 import styles from "./hero.module.css";
+import { useSnapshot } from 'valtio';
+import state from '@/store';
+import useWindowSize from '../hooks/useWindowSize';
 
-const SlideItem = () => {
+import { BsFillPlayFill } from "react-icons/bs";
+
+//#region gsap
+import { Circ, Expo, Power2, gsap } from 'gsap';
+
+const SlideItem = ({ bgCover, cover, title, description, isActive, isNext, isPrev }) => {
+
+    const snap = useSnapshot(state);
+    const { screenWidth } = useWindowSize();
+
+    const styleHeight = screenWidth >= snap.breakPoints.md ? { height: `50svh` } : null
+
+
+    const coverElement = useRef(null);
+    const titleElement = useRef(null);
+    const descriptionElement = useRef(null);
+    const buttonElement = useRef(null);
+
+    useEffect(() => {
+        const tl = gsap.timeline();
+
+        if (isNext || isPrev) {
+            gsap.to(coverElement.current, { opacity: 0, scale: .8 });
+            gsap.to(titleElement.current, { y: -80 });
+            gsap.to(descriptionElement.current, { autoAlpha: 0 })
+        }
+        if (isActive) {
+            tl.to(coverElement.current,
+                {
+                    opacity: 1,
+                    scale: 1,
+                    duration: .6,
+                    delay: .3,
+                    ease: Circ.easeOut
+                }
+            )
+            tl.to(titleElement.current,
+                { y: 0, duration: .6 }
+            )
+        }
+
+    }, [isActive, isNext, isPrev])
+
     return (
         <div className='hero-slide-item relative'>
-            <div className="cover w-full max-md:h-screen">
+            <div className="bg-cover w-full max-md:min-h-[800px] max-md:h-screen" style={styleHeight}>
                 <Image
-                    src={"/images/movie-images/john-wick4.jpg"}
+                    src={bgCover}
                     width={1280}
                     height={720}
                     className='md:w-full max-md:h-full object-cover'
-                    alt='John Wick 4'
+                    alt={title}
                 />
             </div>
 
@@ -20,25 +65,40 @@ const SlideItem = () => {
 
                 <div className="container mx-auto relative z-10 h-full">
 
-                    <div className="flex h-full md:flex-row flex-col gap-20 gap-y-8">
+                    <div className="flex h-full max-md:justify-center md:flex-row flex-col gap-x-2 lg:gap-20 gap-y-8">
 
-                        <div className="cover md:w-4/12 rounded-md overflow-hidden shadow-2xl shadow-black">
-                            <Image
-                                src={"/images/movie-images/john-wick4-cover.jpg"}
-                                width={600}
-                                height={400}
-                                className='h-full w-full md:w-auto object-cover'
-                                alt='John Wick 4'
-                            />
+
+                        <div className="cover-wrapper">
+                            <div ref={coverElement} className="cover max-md:max-h-[500px] md:w-max md:h-full rounded-md overflow-hidden shadow-2xl shadow-black">
+                                <Image
+                                    src={cover}
+                                    width={600}
+                                    height={400}
+                                    className='h-full w-full md:w-auto object-cover'
+                                    alt={title}
+                                />
+                            </div>
+
                         </div>
 
-                        <div className={`content md:w-8/12 ${styles.content} `}>
-                            <h1 className={`${styles.content_title}`}>
-                                John Wick 4
+                        <div className={`content md:w-auto ${styles.content} md:max-w-3xl`}>
+                            <h1 className={`${styles.content_title} inline-block overflow-hidden`}>
+                                <span ref={titleElement} className='inline-block'>
+                                    {title}
+                                </span>
                             </h1>
-                            <p className='max-lg:line-clamp-3'>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci quis natus autem reprehenderit aliquam voluptatibus rem amet placeat. Quis, cupiditate! Odio laudantium porro sapiente consectetur necessitatibus adipisci quia ab qui!
+                            <p ref={descriptionElement} className='max-lg:line-clamp-3 font-medium text-sm'>
+                                {description}
                             </p>
+
+                            <div ref={buttonElement} className="buttons-wrapper mt-auto">
+                                <button className='button-v2 '>
+                                    <BsFillPlayFill />
+                                    <span>
+                                        Şimdi izle
+                                    </span>
+                                </button>
+                            </div>
                         </div>
 
                     </div>
--- src/components/HeroSection/index.jsx
+++ src/components/HeroSection/index.jsx
@@ -5,6 +5,7 @@ import 'swiper/css';
 import { useSnapshot } from 'valtio';
 import state from '@/store';
 import SlideItem from './SlideItem';
+import { Autoplay } from 'swiper';
 
 const HeroSection = () => {
 
@@ -13,17 +14,34 @@ const HeroSection = () => {
 
     return (
         <section className=''>
-            <div className=''>
+            <div>
 
                 <Swiper
                     slidesPerView={1}
+                    speed={1300}
+                    autoplay={{
+                        delay: 6500,
+                    }}
+                    modules={[Autoplay]}
+
                 >
                     {
                         [..."123"].map((data, index) => (
                             <SwiperSlide
                                 key={index}
                             >
-                                <SlideItem />
+                                {({ isActive, isPrev, isNext }) => (
+
+                                    <SlideItem
+                                        isPrev={isPrev}
+                                        isNext={isNext}
+                                        isActive={isActive}
+                                        title={`John Wick ${index}`}
+                                        bgCover={"/images/movie-images/john-wick4.jpg"}
+                                        cover={"/images/movie-images/john-wick4-cover.jpg"}
+                                        description={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci quis natus autem reprehenderit aliquam voluptatibus rem amet placeat. Quis, cupiditate! Odio laudantium porro sapiente consectetur necessitatibus adipisci quia ab qui!"}
+                                    />
+                                )}
                             </SwiperSlide>
                         ))
                     }
